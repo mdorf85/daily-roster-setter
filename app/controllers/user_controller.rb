b7b50f72bfe462a5e8f551270eedb5a08740d681
Michael Dorfman <mdorf85@gmail.com>
2015-01-02 00:46:59 -0500

    gets a request token

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,9 +3,9 @@ class UserController < ApplicationController
   end
 
   def new
-    response = HTTParty.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.teams.roster%20where%20team_key%3D'342.l.100454.t.5'&format=json&diagnostics=true&callback=')
-    puts response
+    # response = HTTParty.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.teams.roster%20where%20team_key%3D'342.l.100454.t.5'&format=json&diagnostics=true&callback=")
+    # puts response
 
-    inspect.params
+
   end
 end
